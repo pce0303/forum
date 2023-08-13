97ef836afefdf511f880a0f8631ea43d83ea24fc
pce0303 <ivychoen@gmail.com>
2023-08-13 16:05:53 +0900

    post router

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const session = require('express-session');
 const db = require('./db');
+const bodyParser = require('body-parser');
 
 const registerRouter = require('./router/register');
 const loginRouter = require('./router/login');
-const bodyParser = require('body-parser');
+const postRouter = require('./router/post');
 
 const port = 3000;
 const app = express();
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/register', registerRouter);
 app.use('/login', loginRouter);
+app.use('/post', postRouter);
 
 app.use(express.static('./styles'));
 
